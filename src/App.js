6e5a03021803885417c6b73c035521630b24ca5a
syogakusya <cy22260@shibaura-it.ac.jp>
2024-04-08 00:48:59 +0900

    station2まで終了

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import { Header } from './components/Header';
-
+import {Threads } from './components/Threads';
 function App() {
   return (
     <div>
       <Header />
-      
+      <Threads />
     </div>
   )
 }
